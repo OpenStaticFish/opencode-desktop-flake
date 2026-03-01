8acd3f686ddd797ca33a62b6c9ef5a13132c8b3c
MichaelFisher1997 <91565606+MichaelFisher1997@users.noreply.github.com>
2026-03-01 06:55:09 +0000

    chore: bump opencode-desktop to 1.2.15

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       
       forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
       
-      version = "1.2.11";
+      version = "1.2.15";
       
     in {
       packages = forEachSystem (pkgs: 
@@ -27,19 +27,19 @@
           platformInfo = {
             x86_64-linux = {
               asset = "opencode-desktop-linux-amd64.deb";
-              hash = "sha256-UmB//8KLPKlvxoJi3cQm0r8FT0JK+wjiaCtBFFFWQQA=";
+              hash = "sha256-TJmFT+ZUtnb3O7LG2xrJJKJBt+VNkTs4jk45j8mnHPE=";
             };
             aarch64-linux = {
               asset = "opencode-desktop-linux-arm64.deb";
-              hash = "sha256-Nk69BqwNBwKx+y/rWcckJkCPLVYUwQG/3wiLgliJVOA=";
+              hash = "sha256-wlEfUjNASbo0xkC4gfwTwmWghkv1rEahKvOLhWJUL2c=";
             };
             aarch64-darwin = {
               asset = "opencode-desktop-darwin-aarch64.app.tar.gz";
-              hash = "sha256-nFH3HGYpm5WZXtkSgBmCwkt+ZEFFeSnkFhmyhaN2Ic8=";
+              hash = "sha256-dWM7gFBeO7S9quefSsK7fElIn2ewTFeIGAXh2Hvm1dA=";
             };
             x86_64-darwin = {
               asset = "opencode-desktop-darwin-x64.app.tar.gz";
-              hash = "sha256-hH6PxNjyeCfhn7/VUc4aaxq/gSINYI58DHQADBaqR3c=";
+              hash = "sha256-b0yYuzGufXGq+0tc7W59QFNXfmEHTLBzHWXX2ruiiIo=";
             };
           };
           
